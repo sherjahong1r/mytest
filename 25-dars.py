1b0b87169c45f3c965bc4f7347a5170173e77aad
Sher_jahongir Tursunmurodov <sherjahongir77@gmail.com>
2025-11-10 23:15:34 -0800

    Add files via upload

---
+++ 25-dars.py
@@ -0,0 +1,93 @@
+"""
+25-dars: Son topish o'yini.
+Created on Mon Sep 29 11:04:25 2025
+@author: Sherjahongir
+"""
+
+
+import random
+
+def sontop(x=10):
+    tasodifiy_son = random.randint(1,x)
+    print(f"Men 1 dan {x} gacha son o'yladim. Topa olasizmi?")
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        taxmin = int(input(">>>"))
+        if taxmin<tasodifiy_son:
+            print("Xato. Men o'ylagan son bundan kattaroq. Yana harakat qiling:")
+        elif taxmin>tasodifiy_son:
+            print("Xato. Men o'ylagan son bundan kichikroq. Yana harakat qiling:")
+        else:
+            break
+    print(f"Tabriklaymiz. {taxminlar} ta taxmin bilan to'g'ri topdingiz!")
+    return taxminlar
+
+print("\n")
+
+def sontop_pc(x=10):
+    input(f"1 dan {x} gacha son o'ylang va istalgan tugmani bosing. Men topaman.")
+    quyi = 1
+    yuqori = x
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        if quyi != yuqori:
+            taxmin = random.randint(quyi,yuqori)
+        else:
+            taxmin = quyi
+        javob = input(f"Siz {taxmin} sonini o'yladingiz: to'g'ri (t),"
+                      f"men o'ylagan son bundan kattaroq (+), yoki kichikroq (-)"
+                      .lower())
+        if javob == "-":
+            yuqori = taxmin - 1
+        elif javob == "+":
+            quyi = taxmin + 1
+        else:
+            break
+    print(f"Men {taxminlar} ta taxmin bilan topdim!")
+    return taxminlar
+
+
+print("\n")
+
+
+def play(x=10):
+    yana = True
+    while yana:
+        taxminlar_user = sontop(x)
+        taxminlar_pc = sontop_pc(x)
+        
+        if taxminlar_user>taxminlar_pc:
+            print("Kampiyuter yutdi!✓ 🏆🏆")
+        elif taxminlar_user<taxminlar_pc:
+            print("Siz yutdingiz!✓ 🏆🏆")
+        else:
+            print("Durrang bo'ldi!")
+        yana = int(input("Yana o'ynaysizmi? Ha(1)/Yo'q(0):"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
